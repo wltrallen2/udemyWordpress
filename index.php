3093737da2bc1e00a994046c45c054129cffcd85
Walter Allen <walterallen2@me.com>
2023-04-14 14:56:07 -0400

    Initial commit (incl. development of theme.json)

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+// When using full-site editing, this file is not used and should be left empty.
+?>
+
+<!-- <!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class('example'); ?>>
+    <?php wp_body_open(); ?>
+    <p>Hello, world</p>
+
+    <?php wp_footer(); ?>
+</body>
+</html> -->
